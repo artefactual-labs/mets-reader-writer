7483de5826d94ed204212417ffe9b52621176a67
Misty De Meo <mdemeo@artefactual.com>
2014-01-31 18:09:25 -0800

    Initial commit

---
+++ mets.py
@@ -0,0 +1,299 @@
+from datetime import datetime
+from random import randint
+
+from lxml import etree
+
+
+NAMESPACES = {
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    "mets": "http://www.loc.gov/METS/",
+    "premis": "info:lc/xmlns/premis-v2",
+    "dcterms": "http://purl.org/dc/terms/",
+    "fits": "http://hul.harvard.edu/ois/xml/ns/fits/fits_output",
+    "xlink": "http://www.w3.org/1999/xlink",
+    "dc": "http://purl.org/dc/elements/1.1/"
+}
+
+# These are convenience copies to allow wrapping the namespaces
+# in lxml's format without confusing extra curly braces
+# in string literals.
+LXML_NAMESPACES = {
+    "dc": "{" + NAMESPACES['dc'] + "}",
+    "dcterms": "{" + NAMESPACES['dcterms'] + "}",
+    "xsi": "{" + NAMESPACES['xsi'] + "}",
+    "mets": "{" + NAMESPACES['mets'] + "}",
+    "premis": "{" + NAMESPACES['premis'] + "}",
+    "fits": "{" + NAMESPACES['fits'] + "}",
+    "xlink": "{" + NAMESPACES['xlink'] + "}"
+}
+
+SCHEMA_LOCATIONS = "http://www.loc.gov/METS/ " + \
+                   "http://www.loc.gov/standards/mets/version18/mets.xsd"
+
+
+class FSEntry(object):
+    """
+    A class representing a filesystem entry - either a file or a directory.
+    Unless otherwise specified, an FSEntry object is assumed to be a file;
+    pass the `type` value as 'directory' to specify that the object is
+    instead a directory.
+
+    When passed to a METSWriter instance, the tree of FSEntry objects will
+    be used to construct the <fileSec> and <structMap> elements of a
+    METS document.
+
+    An FSEntry object must be instantiated with a path as the first
+    argument to the constructor, which represents its path on disk.
+
+    An FSEntry object which is a directory may have one or more children,
+    representing files or directories contained within itself. Directory
+    trees are designed for top-to-bottom traversal. Files cannot have
+    children, and attempting to instantiate a file FSEntry object with
+    children will raise an Exception.
+
+    Any FSEntry object may have one or more metadata entries associated
+    with it; these can take the form of either references to other XML
+    files on disk, which should be wrapped in MDRef objects, or
+    wrapped copies of those XML files, which should be wrapped in
+    MDWrap objects. These can be appended using one of the following
+    methods:
+
+    add_techmd(md, type, mode)     - technical metadata
+    add_digiprovmd(md, type, mode) - digital provenance metadata
+    """
+    def __init__(self, path, children=[], type=u'file',
+                 use='original', id=None):
+        # path can validly be any encoding; if this value needs
+        # to be spliced later on, it's better to treat it as a
+        # bytestring than as actually being encoded text.
+        self.path = str(path)
+        self.type = unicode(type)
+        self.use = use
+        self.id = id
+        self.children = children
+        self.amdsecs = []
+        self.dmdsecs = []
+
+        if type == 'file' and children:
+            raise Exception("Only directory objects can have children")
+
+    def _create_id(prefix):
+        return prefix + '_' + str(randint(1, 999999))
+
+    def _add_metadata_element(self, md, type, mode='mdwrap', category=None):
+        if mode == 'mdwrap':
+            mdsec = MDWrap(md, type)
+        elif mode == 'mdref':
+            mdsec = MDSec(md, type)
+        md_element = etree.Element(category, ID=self._create_id(category))
+        md_element.append(mdsec)
+        if category == 'techMD':
+            self.amdsecs.append(AMDSec(md_element, category))
+        elif category == 'digiProvMD':
+            self.dmdsecs.append(DMDSec(md_element, category))
+
+    def add_techmd(self, md, type, mode=None):
+        self._add_metadata_element(md, type, mode, category='techMD')
+
+    def add_digiprovmd(self, md, type, mode='mdwrap'):
+        self._add_metadata_element(md, type, mode, category='digiProvMD')
+
+
+class MDRef(object):
+    """
+    An object representing an external XML document, typically associated
+    with an FSEntry object.
+
+    The `target` attribute is a path to the external document. MDRef does
+    not validate the existence of this target.
+
+    The `type` argument must be a string representing the type of XML
+    document being enclosed. Examples include "PREMIS:OBJECT" and
+    "PREMIS:EVENT".
+    """
+    def __init__(self, target, type):
+        self.target = target
+        self.type = type
+        self.id = None
+
+    def serialize(self):
+        el = etree.Element('mdSec', MDTYPE=self.type)
+
+        return el
+
+
+class MDWrap(object):
+    """
+    An object representing an XML document enclosed in a METS document.
+    The entirety of the XML document will be included; to reference an
+    external document, use the MDRef class.
+
+    The `document` argument must contain a string copy of the document,
+    and will be parsed into an ElementTree at the time of instantiation.
+
+    The `type` argument must be a string representing the type of XML
+    document being enclosed. Examples include "PREMIS:OBJECT" and
+    "PREMIS:EVENT".
+    """
+    def __init__(self, document, type):
+        self.document = etree.fromstring(document)
+        self.type = type
+        self.id = None
+
+    def serialize(self):
+        el = etree.Element('mdWrap', MDTYPE=self.type)
+        xmldata = etree.SubElement(el, 'xmlData')
+        xmldata.append(self.document)
+
+        return el
+
+
+class MDSec(object):
+    type = None
+
+    def __init__(self, contents, type):
+        self.contents = contents
+        self.type = type
+        self.number = str(randint(1, 999999))
+
+    def id_string(self):
+        # e.g., amdSec_1
+        return self.__class__.type + '_' + self.number
+
+    def serialize(self):
+        el = etree.Element(self.__class__.type, ID=self.id_string())
+        container = etree.SubElement(el, self.type)
+        container.append(self.contents.serialize())
+
+        return el
+
+
+class AMDSec(MDSec):
+    """
+    An object representing a section of administrative metadata in a
+    document.
+
+    This is ordinarily created by METSWriter instances and does not
+    have to be instantiated directly.
+    """
+
+    type = 'amdSec'
+
+
+class DMDSec(MDSec):
+    """
+    An object representing a section of descriptive metadata in a
+    document.
+
+    This is ordinarily created by METSWriter instances and does not
+    have to be instantiated directly.
+    """
+
+    type = 'dmdSec'
+
+
+class FileGrp(object):
+    def __init__(self, file):
+        self.file = file
+
+    def serialize(self):
+        el = etree.Element('fileGrp', use=file.use)
+        # TODO: GROUPID? ADMID?
+        file_el = etree.SubElement(el, 'file', ID=file.id)
+        attrib = {
+            LXML_NAMESPACES['xlink']+'href': file.path,
+            'LOCTYPE': 'OTHER',
+            'OTHERLOCTYPE': 'SYSTEM'
+        }
+        etree.SubElement(file_el, 'Flocat', attrib=attrib)
+
+        return el
+
+
+class FileSec(object):
+    def __init__(self, filegrps):
+        self.filegrps = filegrps
+
+    def serialize(self):
+        el = etree.Element('fileSec')
+        for filegrp in self.filegrps:
+            el.append(filegrp.serialize())
+
+        return el
+
+
+class METSWriter(object):
+    def __init__(self):
+        self.tree = etree.ElementTree()
+        # Only root-level elements are stored, since the rest
+        # can be inferred via their #children attribute
+        self.root_elements = []
+        self.dmdsecs = []
+        self.amdsecs = []
+
+    def _document_root(self):
+        nsmap = {None: NAMESPACES['mets']}
+        attrib = {
+            '{}schemaLocation'.format(LXML_NAMESPACES['xsi']):
+            SCHEMA_LOCATIONS
+        }
+        return etree.Element('mets', nsmap=nsmap, attrib=attrib)
+
+    def _mets_header(self):
+        date = datetime.utcnow().isoformat('T')
+        return etree.Element('metsHdr', CREATEDATE=date)
+
+    def _structmap(self):
+        pass
+
+    def _filesec(self):
+        filegrps = [FileGrp(file) for file in self.files]
+        return FileSec(filegrps)
+
+    def _append_file_properties(self, file):
+        for amdsec in file.amdsecs:
+            self.amdsec_count += 1
+            amdsec.id = self.amdsec_count
+            self.amdsecs.append(amdsec)
+        for dmdsec in file.dmdsecs:
+            self.dmdsec_count += 1
+            dmdsec.id = self.dmdsec_count
+            self.dmdsecs.append(dmdsec)
+
+    def _mdsec_elements(self):
+        elements = []
+        for amdsec in self.amdsecs:
+            elements.append(amdsec.serialize())
+        for dmdsec in self.dmdsecs:
+            elements.append(dmdsec.serialize())
+
+        return elements
+
+    def append_file(self, file):
+        """
+        Adds an FSEntry object to this METS document's tree. Any of the
+        represented object's children will also be added to the document.
+
+        A given FSEntry object can only be included in a document once,
+        and any attempt to add an object the second time will be ignored.
+        """
+
+        if file in self.root_elements:
+            return
+
+        self.files.append(file)
+        self._append_file_properties(file)
+
+        # children are *not* included in the root properties list,
+        # because anything that includes children will already
+        # allow traversal down the tree.
+        # However, we *do* require the full set of mdSecs at this time.
+        for child in file.children:
+            self._append_file_properties(child)
+
+    def serialize(self):
+        """
+        Returns an ElementTree object representing this document.
+        """
+        root = self._document_root()
+        root.append(self._mets_header())
---
+++ test_mets.py
@@ -0,0 +1,63 @@
+from lxml import etree
+import mets
+
+
+def test_mets_root():
+    mw = mets.METSWriter()
+    root = mw._document_root()
+    location = "http://www.loc.gov/METS/ http://www.loc.gov/standards/mets/version18/mets.xsd"
+    assert root.tag == 'mets'
+    assert root.attrib[mets.LXML_NAMESPACES['xsi']+'schemaLocation'] == location
+    assert root.nsmap[None] == 'http://www.loc.gov/METS/'
+
+
+def test_mets_header():
+    mw = mets.METSWriter()
+    header = mw._mets_header()
+    assert header.tag == 'metsHdr'
+    assert header.attrib['CREATEDATE']
+
+
+def test_mdsec_identifier():
+    # should be in the format 'amdSec_1'
+    amdsec = mets.AMDSec('foo', 'techMD')
+    assert amdsec.id_string()
+
+
+def test_mdwrap():
+    mdwrap = mets.MDWrap('<foo/>', 'PREMIS:DUMMY')
+    mdwrapped = mdwrap.serialize()
+
+    target = '<mdWrap MDTYPE="PREMIS:DUMMY"><xmlData><foo/></xmlData></mdWrap>'
+
+    assert mdwrapped.tag == 'mdWrap'
+    assert mdwrap.document.tag == 'foo'
+    assert etree.tostring(mdwrapped) == target
+
+
+# def test_mdref():
+#     mdref = mets.MDRef('path/to/file.txt', 'PREMIS:DUMMY')
+#     mdreffed = mdref.serialize()
+
+#     target = '<mdRef MDTYPE="PREMIS:DUMMY"></mdRef>'
+
+
+def test_mdsec_list_production():
+    mw = mets.METSWriter()
+    xml = mets.MDWrap('<foo/>', 'techMD')
+    amdsec = mets.AMDSec(xml, 'techMD')
+    mw.amdsecs.append(amdsec)
+    elements = mw._mdsec_elements()
+
+    assert isinstance(elements, list)
+    assert len(elements) == 1
+    assert isinstance(elements[0], etree._Element)
+    assert elements[0].tag == 'amdSec'
+
+    xml2 = mets.MDWrap('<bar/>', 'digiProvMD')
+    dmdsec = mets.DMDSec(xml2, 'digiProvMD')
+    mw.dmdsecs.append(dmdsec)
+    elements = mw._mdsec_elements()
+
+    assert len(elements) == 2
+    assert elements[1].tag == 'dmdSec'
