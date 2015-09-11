984c59db5b63de77ab23ee4d8d23508997925936
Holly Becker <becker.holly@gmail.com>
2015-09-11 16:12:31 -0700

    Add tests

--- tests/test_fsentry.py
+++ tests/test_fsentry.py
@@ -8,25 +8,58 @@ import metsrw
 class TestFSEntry(TestCase):
     """ Test FSEntry class. """
 
-    def test_file_id(self):
+    def test_file_id_directory(self):
+        """ It should have no file ID. """
         d = metsrw.FSEntry('dir', type='Directory')
         assert d.file_id() is None
+
+    def test_file_id_no_uuid(self):
+        """ It should raise an exception with no file UUID. """
         f = metsrw.FSEntry('level1.txt')
         with pytest.raises(metsrw.MetsError):
             f.file_id()
+
+    def test_file_id_success(self):
+        """ It should return a file ID. """
         file_uuid = str(uuid.uuid4())
         f = metsrw.FSEntry('level1.txt', file_uuid=file_uuid)
         assert f.file_id() == 'file-' + file_uuid
 
-    def test_group_id(self):
+    def test_group_id_no_uuid(self):
+        """ It should return no group ID. """
         f = metsrw.FSEntry('level1.txt')
         assert f.group_id() is None
+
+    def test_group_id_uuid(self):
+        """ It should return a group ID. """
         file_uuid = str(uuid.uuid4())
         f = metsrw.FSEntry('level1.txt', file_uuid=file_uuid)
         assert f.group_id() == 'Group-' + file_uuid
+
+    def test_group_id_derived(self):
+        """ It should return the group ID for the derived from file. """
+        file_uuid = str(uuid.uuid4())
+        f = metsrw.FSEntry('level1.txt', file_uuid=file_uuid)
         derived = metsrw.FSEntry('level3.txt', file_uuid=str(uuid.uuid4()), derived_from=f)
         assert derived.group_id() == 'Group-' + file_uuid
         assert derived.group_id() == f.group_id()
+
+    def test_admids(self):
+        """ It should return 0 or 1 amdSecs. """
+        f = metsrw.FSEntry('file1.txt', file_uuid=str(uuid.uuid4()))
+        assert len(f.admids()) == 0
+        f.add_premis_object('<premis>object</premis>')
+        assert len(f.admids()) == 1
+        f.add_premis_event('<premis>event</premis>')
+        # Can only have one amdSec
+        assert len(f.admids()) == 1
+
+    def test_dmdids(self):
+        """ It should return a DMDID for each dmdSec. """
+        f = metsrw.FSEntry('file1.txt', file_uuid=str(uuid.uuid4()))
+        assert len(f.dmdids()) == 0
+        f.add_dublin_core('<dc />')
+        assert len(f.dmdids()) == 1
 
     def test_add_metadata_to_fsentry(self):
         f1 = metsrw.FSEntry('file1.txt', file_uuid=str(uuid.uuid4()))
@@ -36,8 +69,8 @@ class TestFSEntry(TestCase):
         assert f1.dmdsecs[0].subsection == 'dmdSec'
         assert f1.dmdsecs[0].contents.mdtype == 'DC'
 
+        # Can only have 1 amdSec, so subsequent subsections are children of AMDSec
         f1.add_premis_object('<premis>object</premis>')
-
         assert f1.amdsecs
         assert f1.amdsecs[0].subsections
         assert f1.amdsecs[0].subsections[0].subsection == 'techMD'
