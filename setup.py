5f9e786c55510c456fe4a9fd18e19dcee0ae2522
Holly Becker <hbecker@artefactual.com>
2016-10-13 13:55:30 -0700

    Version: Update to 0.1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='metsrw',
-    version='0.0.1',
+    version='0.1.0',
 
     description='Library for dealing with METS files.',
     long_description=long_description,
