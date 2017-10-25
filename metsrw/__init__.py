2f672328bbc8f6eb94a62bb7fb7e4b67a1ab0913
Joel Dunham <jrwdunham@gmail.com>
2017-10-25 08:00:22 -0700

    Flake8: fix E402 module level import not at top of file

--- metsrw/__init__.py
+++ metsrw/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import
-
 import logging
-LOGGER = logging.getLogger(__name__)
-LOGGER.addHandler(logging.NullHandler())
 
 from .exceptions import *
 from .fsentry import *
 from .metadata import *
 from .mets import *
 from .utils import *
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
