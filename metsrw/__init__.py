0b8cfe8315bbbd8a96de26d50466c8a21aa4f7a5
Joel Dunham <jrwdunham@gmail.com>
2017-10-25 08:15:53 -0700

    Flake8: fix F401 and F403 (import errors)
    - F403 'from .exceptions import *' used; unable to detect undefined names
    - F401 '.exceptions.*' imported but unused

--- metsrw/__init__.py
+++ metsrw/__init__.py
@@ -1,11 +1,21 @@
 from __future__ import absolute_import
 import logging
 
-from .exceptions import *
-from .fsentry import *
-from .metadata import *
-from .mets import *
-from .utils import *
+from .exceptions import MetsError, ParseError
+from .fsentry import FSEntry
+from .metadata import AMDSec, SubSection, MDRef, MDWrap
+from .mets import METSDocument
+from .utils import (
+    NAMESPACES,
+    SCHEMA_LOCATIONS,
+    lxmlns,
+    FILE_ID_PREFIX,
+    GROUP_ID_PREFIX
+)
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(logging.NullHandler())
+
+__all__ = ['MetsError', 'ParseError', 'FSEntry', 'AMDSec', 'SubSection',
+           'MDRef', 'MDWrap', 'METSDocument', 'NAMESPACES', 'SCHEMA_LOCATIONS',
+           'lxmlns', 'FILE_ID_PREFIX', 'GROUP_ID_PREFIX']
