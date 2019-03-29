67b69e15a9e160959118474a7d1aa44b470ae634
Cole Maclean <hi@cole.io>
2019-03-29 15:18:26 -0700

    Add docs build to CI

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.doctest"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -324,3 +324,9 @@ epub_exclude_files = ["search.html"]
 
 # If false, no index is generated.
 # epub_use_index = True
+
+doctest_global_setup = """
+import uuid
+
+import metsrw
+"""
