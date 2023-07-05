9fcaee19033a3cdd348f48fb799fd5a5c6da3b5e
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2023-07-05 13:55:46 +0000

    Use local XML schemas in validation tests
    This prevents accessing the loc.gov URLs during those tests.

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pathlib
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def set_xml_catalog_files(monkeypatch):
+    """Use local XML schemas for validation."""
+    monkeypatch.setenv(
+        "XML_CATALOG_FILES",
+        str(pathlib.Path(__file__).parent / "assets" / "catalog.xml"),
+    )
